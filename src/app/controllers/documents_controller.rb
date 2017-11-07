ad34123e305dd68fdcc37ad46c5766cca3fa70c6
KaloyanDraganov <kaloyan.draganov.15@aberdeen.ac.uk>
2017-11-07 15:55:32 +0000

    comments

--- src/app/controllers/documents_controller.rb
+++ src/app/controllers/documents_controller.rb
@@ -39,7 +39,6 @@ class DocumentsController < ApplicationController
       and params[:p].to_i > 0 and  params[:p].to_i < 1000000
       # Store the volume and page from the input
       @volume, @page = params[:v].to_i, params[:p].to_i
-
       if params[:searchID]
         @searchID = 'searchID'
       end
@@ -77,6 +76,7 @@ class DocumentsController < ApplicationController
       @volume, @page = params[:v].to_i, params[:p].to_i
       # Select Documents
       @documents = Search.order(:paragraph).where('volume' => @volume).rewhere('page' => @page)
+      @comments = Comment.where("search_volume = ? AND search_page = ?", @volume, @page)
       if @documents.length > 0
         # Select image
         page_image = PageImage.find_by_volume_and_page(@volume, @page)
